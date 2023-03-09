feea7e7c6470a26faee22a6b92e88c0b7e3c7d6e
Arnau Comajuncosa <arnau.comajuncosa@irbbarcelona.org>
2023-03-09 10:21:40 +0100

    compiled rDock
    Former-commit-id: 1f39e6aef38bcbc8669e97237e07d94b0ac4f882

--- code/utils/pocketvec_utils.py
+++ code/utils/pocketvec_utils.py
@@ -39,3 +39,56 @@ def prepare_pdb(infile, outfile, logfile, moebatch = "/aloy/home/acomajuncosa/pr
     else:
         sys.stderr.write(str(stdout) + "\n\n")
         sys.stderr.write(str(stderr) + "\n\n")
+        
+        
+        
+        
+def create_parameter_file(outfile, path_to_st, path_to_ctr, radius=str(12.0)):
+    """
+    Create structure parameter file.
+    
+    Args:
+    
+        outfile (str): Path to output structure parameter file (rDock-PRM format)
+        path_to_st (str): Path to receptor (MOL2 format).
+        path_to_ctr (str): Path to pocket centroid (SD format). 
+        radius (float/str): Pocket radius (default 12.0).
+        
+    
+    """
+    
+    text = """RBT_PARAMETER_FILE_V1.00
+TITLE Change this if you want
+
+RECEPTOR_FILE """ + path_to_st + """
+RECEPTOR_FLEX 3.0
+
+#################################################################
+## CAVITY DEFINITION: REFERENCE LIGAND METHOD
+#################################################################
+SECTION MAPPER
+    SITE_MAPPER RbtLigandSiteMapper
+    REF_MOL """ + path_to_ctr + """
+    RADIUS """ + radius + """
+    SMALL_SPHERE 1.0
+    MIN_VOLUME 100
+    MAX_CAVITIES 1
+END_SECTION
+
+
+SECTION CAVITY
+    SCORING_FUNCTION RbtCavityGridSF
+    WEIGHT 1.0
+END_SECTION
+
+#################################################
+# WARNING!!! WE ARE DOING RIGID LIGAND DOCKING!!
+#################################################
+SECTION LIGAND
+    TRANS_MODE FREE
+    ROT_MODE FREE
+    DIHEDRAL_MODE """ + "FIXED" + """
+END_SECTION"""
+    
+    with open(outfile, "w") as f:
+        f.write(text)
